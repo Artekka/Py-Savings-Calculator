b6202b18b5758fc135a69fd41ead348d9bb4af10
Artekka <F1rmW4reInc@gmail.com>
2017-11-28 16:42:48 -0800

    Decided on CamelCase, Improved .format
    Wasn't sure whether to go with snake or camel. Sticking with camel for
    now and will try to keep var names short enough to still be descriptive.
    
    Implemented formatting for numbers in order to display numbers as
    accurately as they need to be (ie 12.3%, or $1,234.56) along with commas
    to make larger numbers easier to interpret.

--- SavingsCalculator.py
+++ SavingsCalculator.py
@@ -6,20 +6,20 @@ def calculateSavings():
     savings = 0
     annualIncome = float(weeklyIncome * weeks)
     taxBrackets = [(418400, 0.396), (416700, 0.35), (191650, 0.33), (91900, 0.28), (37950, 0.25), (9325, 0.15), (0, 0.10)] 
-    def determine_taxBracket(income):
+    def findTaxBracket(income):
       return [tax for (tax_limits, tax) in taxBrackets if income > tax_limits][0]
-    user_taxBracket = determine_taxBracket(annualIncome)
-    taxedAmount = weeklyIncome * user_taxBracket
+    userTaxBracket = findTaxBracket(annualIncome)
+    taxedAmount = weeklyIncome * userTaxBracket
     netIncome = weeklyIncome - taxedAmount
     print("What percentage (%) of your income would you like to save each week?")
     wishToSave = int(input())
     savedPercent = float(wishToSave / 100)
     print("You have chosen to save: " + str(float(wishToSave)) + "%")
-    print("Your net income per week is: $" + str(int(netIncome)))
-    print("Your tax bracket is: " + str(float(user_taxBracket)*100) + "%")
+    print("Your net income per week is: ${:,}".format(int(netIncome)))
+    print("Your tax bracket is: {:.1f}%".format(userTaxBracket * 100))
     x = 0
     for x in range(1, weeks+1):
         savings += float(netIncome)*savedPercent
     netIncome *= weeks
-    print("In {0} weeks, you have saved ${1} from ${2}.".format(str(x),str(int(savings)), str(int(netIncome))))
+    print("In {0} weeks you will have saved ${1:,} from ${2:,}.".format(str(x),int(savings),int(netIncome)))
 calculateSavings()
