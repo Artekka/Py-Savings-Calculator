cf5e97bbe75fb280039d6fcf83dc0165423976f6
Artekka <Artekka@users.noreply.github.com>
2017-12-01 14:04:03 -0800

    Started taxableIncome
    Finish at home

--- SavingsCalculator.py
+++ SavingsCalculator.py
@@ -24,4 +24,17 @@ def findTaxBracket(income):
   taxBrackets = [(418400, 0.396), (416700, 0.35), (191650, 0.33), (91900, 0.28), (37950, 0.25), (9325, 0.15), (0, 0.10)] 
   return [tax for (tax_limits, tax) in taxBrackets if income > tax_limits][0]
 
+def taxableIncome(taxBracket, income):
+  taxes = [
+    (0.396, income - (121505.25 + (0.396 * (418400 - 121505.25)))),
+    (0.35, income - 120910.25 + (0.35 * (416700 - 120910.25))),
+    (0.33, income - 46643.75 + (0.33 * (191650 - 46643.75))),
+    (0.28, income - 18713.75 + (0.28 * (91900 - 18713.75))),
+    (0.25, income - 5226.25 + (0.25 *(37950 - 5226.25))),
+    (0.15, income - 932.50 + (0.15 * (9325 - 932.50))),
+    (0.10, income - 0)
+    ]
+
+  return 
+
 calculateSavings()
